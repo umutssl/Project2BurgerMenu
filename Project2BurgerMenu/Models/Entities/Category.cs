b3aef36667f1e87f323bfccf091c4b7c0c415b5b
Umut Süslü <susluumut541@gmail.com>
2024-10-04 15:21:32 +0300

    Add project files.

---
+++ Project2BurgerMenu/Models/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project2BurgerMenu.Models.Entities
+{
+    public class Category
+    {
+        
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
